9af4d85d0e15c62f25da104e465afc919fb96679
Isaias Gregory <106548529+Lightgreg@users.noreply.github.com>
2022-06-15 19:36:14 -0300

    5° entrega...

---
+++ script.js
@@ -0,0 +1,41 @@
+const refrigerante_x = 3
+const macarrao_x = 4
+const ervilha_x = 3
+const arroz_x = 3
+const feijao_x = 2
+const vinho_x = 3
+
+let valor_refrigerante = prompt("preço refrigerante")
+let refrigerante = (valor_refrigerante * refrigerante_x)
+
+let valor_macarrao = prompt("preço macarrao")
+let macarrao = (valor_macarrao * macarrao_x)
+
+let valor_ervilha = prompt("preço ervilha")
+let ervilha = (valor_ervilha * ervilha_x)
+
+let valor_arroz = prompt("preço arroz")
+let arroz = (valor_arroz * arroz_x)
+
+let valor_feijao = prompt("preço feijao")
+let feijao = (valor_feijao * feijao_x)
+
+let valor_vinho = prompt("preço vinho")
+let vinho = (valor_vinho * vinho_x)
+
+let total = (refrigerante+macarrao+ervilha+arroz+feijao+vinho)
+let metade = (total/2)
+
+
+if(total%2 == 0){
+    eu = metade.toFixed(2)
+    amigo = metade.toFixed(2)
+    alert("valor que eu vou pagar = "+eu+" e o que voce vai pagar = "+amigo)
+  }else{
+    eu = (metade-vinho).toFixed(2)
+    amigo = (metade+vinho).toFixed(2)  
+    alert("valor que eu vou pagar = "+eu+" e o que voce vai pagar = "+amigo)
+}
+
+  
+ 
