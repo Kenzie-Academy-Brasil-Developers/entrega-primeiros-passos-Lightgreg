3f5e035980832e1245d4e9dc4964e0d9dc77dfdd
Isaias Gregory <106548529+Lightgreg@users.noreply.github.com>
2022-06-20 15:35:21 -0300

    correçao....

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
-const refrigerante_x = 3
+const refrigerante_x = 4
 const macarrao_x = 4
 const ervilha_x = 3
 const arroz_x = 3
 const feijao_x = 2
 const vinho_x = 3
 
-let valor_refrigerante = prompt("preço refrigerante")
-let refrigerante = (valor_refrigerante * refrigerante_x)
+let refrigerante = 11.99
+let totalrefrigerante = (refrigerante * refrigerante_x)
 
-let valor_macarrao = prompt("preço macarrao")
-let macarrao = (valor_macarrao * macarrao_x)
+let macarrao = 6.99
+let totalmacarrao = (macarrao * macarrao_x)
 
-let valor_ervilha = prompt("preço ervilha")
-let ervilha = (valor_ervilha * ervilha_x)
+let ervilha = 6.99
+let totalervilha = (ervilha * ervilha_x)
 
-let valor_arroz = prompt("preço arroz")
-let arroz = (valor_arroz * arroz_x)
+let arroz = 22.99
+let totalarroz = (arroz * arroz_x)
 
-let valor_feijao = prompt("preço feijao")
-let feijao = (valor_feijao * feijao_x)
+let feijao = 11.89
+let totalfeijao = (feijao * feijao_x)
 
-let valor_vinho = prompt("preço vinho")
-let vinho = (valor_vinho * vinho_x)
+let vinho = 70.00
+let totalvinho = (vinho * vinho_x)
 
-let total = parseInt(refrigerante+macarrao+ervilha+arroz+feijao+vinho)
-let total2 = parseInt(refrigerante+macarrao+ervilha+arroz+feijao)
-let metade = parseInt(total/2)
+let total =(totalrefrigerante+totalmacarrao+totalervilha+totalarroz+totalfeijao+totalvinho)
+alert("esse é o total "+total)
 
-
-if(total%2 == 0){
-    eu = metade.toFixed(2)
-    amigo = metade.toFixed(2)
+let  somatotal= total * 100;
+  if(somatotal % 2 === 0){
+    eu = (total/2).toFixed(2)
+    amigo =(total/2).toFixed(2)
     alert("valor que eu vou pagar = "+eu+" e o que voce vai pagar = "+amigo)
   }else{
-    eu = (total2).toFixed(2)
-    amigo = (total2+vinho).toFixed(2)  
+    eu = ((total-totalvinho)/2).toFixed(2)
+    amigo = ((total-totalvinho)/2+totalvinho).toFixed(2)  
     alert("valor que eu vou pagar = "+eu+" e o que voce vai pagar = "+amigo)
 }
 
   // let produtos = (refrigerante||macarrao||ervilha||arroz||feijao||vinho)
  
+  // let refrigerante = prompt("preço refrigerante")
+  // let refrigerante = (refrigerante * refrigerante_x)
+  
+  // let macarrao = prompt("preço macarrao")
+  // let macarrao = (macarrao * macarrao_x)
+  
+  // let ervilha = prompt("preço ervilha")
+  // let ervilha = (ervilha * ervilha_x)
+  
+  // let arroz = prompt("preço arroz")
+  // let arroz = (arroz * arroz_x)
+  
+  // let feijao = prompt("preço feijao")
+  // let feijao = (feijao * feijao_x)
+  
+  // let vinho = prompt("preço vinho")
+  // let vinho = (vinho * vinho_x)
